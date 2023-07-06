58d72c9df7cecf2365ea9d2c3d6a34e63411cec4
hatadurdyyev <hatadurdyyev@gmail.com>
2023-07-06 22:44:57 +0300

    framework

---
+++ src/test/java/pages/LessonMngPage.java
@@ -0,0 +1,24 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class LessonMngPage {
+
+    public LessonMngPage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    /*  -- sample --
+    @FindBy(partialLinkText = "Login")
+    public WebElement homeLoginLink;
+     */
+    @FindBy(id = "controlled-tab-example-tab-lessonsList")
+    public WebElement lessonButton;
+
+
+
+
+}
