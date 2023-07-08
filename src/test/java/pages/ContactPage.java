1e8c4dfa103f4ab6b738119cd0276fb0f96eb83b
agnieszkaczarnocka <aga.czarnocka@yahoo.co.uk>
2023-07-09 00:07:35 +0100

    ConctactPage

--- src/test/java/pages/ContactPage.java
+++ src/test/java/pages/ContactPage.java
@@ -1,5 +1,7 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
 
@@ -13,6 +15,16 @@ public class ContactPage {
     @FindBy(partialLinkText = "Login")
     public WebElement homeLoginLink;
      */
+    @FindBy(id = "name")
+    public WebElement yourname;
+    @FindBy(id ="email")
+    public WebElement email;
+    @FindBy(id = "subject")
+    public WebElement subject;
+    @FindBy(id ="message")
+    public WebElement message;
+    @FindBy(xpath = "(//button[@type ='button'])[3]")
+    public WebElement safeMessage;
 
 
 
