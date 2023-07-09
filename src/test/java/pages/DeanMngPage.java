9b0b195fd068481992a307d2f2675e888e873853
Sumeyra Simsek <sumyr.smsek@gmail.com>
2023-07-09 14:05:15 +0100

    US_05

--- src/test/java/pages/DeanMngPage.java
+++ src/test/java/pages/DeanMngPage.java
@@ -3,14 +3,104 @@ package pages;
 import org.openqa.selenium.*;
 import org.openqa.selenium.support.*;
 import utilities.Driver;
+import org.openqa.selenium.support.PageFactory;
+
 
 public class DeanMngPage {
 
     public DeanMngPage() {
-        PageFactory.initElements(Driver.getDriver(), this);
+        PageFactory.initElements(Driver.getDriver(),this);
     }
 
+    @FindBy(xpath = "//*[@href='/login']")
+    public WebElement loginIconForDnMng;
 
+    @FindBy(xpath = "//input[@id='username']")
+    public WebElement loginAdminUsernameForDnMng;
+
+    @FindBy(xpath = "//input[@id='password']")
+    public WebElement loginAdminPasswordForDnMng;
+
+    @FindBy(xpath = "//button[@class='fw-semibold btn btn-primary' and @type='button']")
+    public WebElement loginButtonForDnMng;
+
+    @FindBy(xpath = "//button[.='Menu']")
+    public WebElement menuButton;
+
+    @FindBy(xpath = "//a[.='Dean Management']")
+    public WebElement deanMngOptionButton;
+
+    @FindBy(xpath = "//table//tr[4]//td[6]")
+    public WebElement deanFromList;
+
+    @FindBy(xpath = "(//input[@id='name'])[2]")
+    public WebElement newNameInputEditPage;
+
+    @FindBy(xpath = "(//input[@id='surname'])[2]")
+    public WebElement newSurnameInputEditPage;
+
+    @FindBy(xpath = "(//input[@id='birthPlace'])[2]")
+    public WebElement newBirthPlaceInput;
+
+    @FindBy(xpath = "(//input[@value='FEMALE'])[2]")
+    public WebElement genderFemaleRadioButton;
+
+    @FindBy(xpath = "(//input[@value='MALE'])[2]")
+    public WebElement genderMaleRadioButton;
+
+    @FindBy(xpath = "(//input[@id='birthDay'])[2]")
+    public WebElement newDateOfBirthInput;
+
+    @FindBy(xpath = "(//input[@id='phoneNumber'])[2]")
+    public WebElement newPhoneNumberInput;
+
+    @FindBy(xpath = "(//input[@id='ssn'])[2]")
+    public WebElement newSsnNumberInput;
+
+    @FindBy(xpath = "(//input[@id='username'])[2]")
+    public WebElement newUsernameInputEditPage;
+
+    @FindBy(xpath = "(//input[@id='password'])[2]")
+    public WebElement newPasswordInputEditPage;
+
+    @FindBy(xpath = "(//button[.='Submit'])[2]")
+    public WebElement editPageSubmitButton;
+
+    @FindBy(xpath = "//div[text()='Dean updated Successful']")
+    public WebElement deanUpdtScssMessage;
+
+    @FindBy(xpath = "(//button[@aria-label='Close'])[3]")
+    public WebElement crossSignInEditPage;
+
+    @FindBy(xpath = "//h3[text()='Dean Management']")
+    public WebElement deanManagementTitleText;
+
+    @FindBy(xpath = "//h5[text()='Dean List']")
+    public WebElement deanListTitleText;
+
+    @FindBy(xpath = "(//span[@aria-hidden='true'])[3]")
+    public WebElement nextPageInDeanList;
+
+    @FindBy(xpath = "(//span[@aria-hidden='true'])[4]")
+    public WebElement theLastPageInDeanList;
+
+    @FindBy(xpath = "(//span[@aria-hidden='true'])[1]")
+    public WebElement theFirstPageInDeanListButton;
+
+    @FindBy(xpath = "//th[.='Name']")
+    public WebElement nameColumn;
+
+    @FindBy(xpath = "//th[.='Gender']")
+    public WebElement genderColumn;
+
+    @FindBy(xpath = "//th[.='Phone Number']")
+    public WebElement phone_number;
+
+    @FindBy(xpath = "//th[.='Ssn']")
+    public WebElement SsnColumn;
+
+    @FindBy(xpath = "//th[.='User Name']")
+    public WebElement usernameColumn;
 
 
 
---
+++ src/test/resources/features/us_05/TC01.feature
@@ -0,0 +1,35 @@
+@dean_mng_login
+Feature: admin_goes_to_dean_management_page
+
+  Background:
+  Scenario: admin_goes_to_login_page
+    Given admin navigates to url "https://managementonschools.com/"
+    When admin clicks to login icon
+    And admin enters username "Project09Admin"
+    And admin enters password "12345678"
+    And admin click to login button
+
+    Scenario Outline:TC02_admin_should_update_deans
+      Given admin clicks menu button
+      When admin clicks dean management option
+      And admin clicks on edit button
+      And admin enters dean name "<deanName>"
+      And admin enters dean surname "<deanSurname>"
+      And admin enters dean birthplace "<deanBirthPlace>"
+      And admin selects dean gender
+      And admin enters dean dateofbirth "<deanDateOfBirth>"
+      And admin enters dean phonenumber "<deanPhoneNumber>"
+      And admin enters dean SSN "<deanSSN>"
+      And admin enters dean username "<deanUsername>"
+      And admin enters dean password "<deanPassword>"
+      And admin clicks submit button in edit page
+      Then assert "Dean Successfully Updated" message displayed
+      And admin clicks on cross sign
+      And close the website
+
+      Examples:
+        | deanName | deanSurname | deanBirthPlace | deanDateOfBirth | deanPhoneNumber | deanSSN     | deanUsername | deanPassword |
+        | Jeremy   | Doe         | London         | 05/05/2010      | 198-182-3914    | 182-32-4621 | Jeremy001    | 12345678     |
+
+
+
