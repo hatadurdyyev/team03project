1e8975eed34c12984758a6f074200f337e4f1172
GulerBayri <102733278+GulerBayri@users.noreply.github.com>
2023-07-16 14:07:59 +0200

    last updated version3

--- src/test/java/stepdefinitions/CommonStepDefinitions.java
+++ src/test/java/stepdefinitions/CommonStepDefinitions.java
@@ -1,7 +1,15 @@
 package stepdefinitions;
 
+import io.cucumber.java.en.Then;
+import utilities.Driver;
+
 public class CommonStepDefinitions {
     // GooglePage googlePage = new GooglePage();
 
+    @Then("close the application")
+    public void close_the_application() {
 
+        Driver.closeDriver();
+
+    }
 }
--- src/test/java/stepdefinitions/US_08_StepDefs/UI_StepDefs.java
+++ src/test/java/stepdefinitions/US_08_StepDefs/UI_StepDefs.java
@@ -8,13 +8,16 @@ import io.cucumber.java.en.When;
 import pages.LoginPage;
 import pages.ViceDeanMngPage;
 import utilities.Driver;
+import utilities.MediaUtils;
 import utilities.WaitUtils;
+
+import java.io.IOException;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static utilities.Driver.getDriver;
 
-public class ViceDeanMngCreateLesson {
+public class UI_StepDefs {
     ViceDeanMngPage viceDeanMngPage = new ViceDeanMngPage();
     LoginPage loginPage = new LoginPage();
 
@@ -30,21 +33,23 @@ public class ViceDeanMngCreateLesson {
     }
 
     @Given("user enters the vice deans {string} and {string}")
-    public void user_enters_the_vice_deans_username_and_password(String username, String password) {
+    public void user_enters_the_vice_deans_username_and_password(String username, String password) throws IOException {
         WaitUtils.waitFor(1);
 
         viceDeanMngPage.viceDeanUserName.sendKeys("SevgiKutlu09");
         WaitUtils.waitFor(1);
         viceDeanMngPage.viceDeanPassWord.sendKeys("SevgiKutlu09123");
         WaitUtils.waitFor(1);
+        MediaUtils.takeScreenshotOfTheEntirePage();
         viceDeanMngPage.loginSubmitButton.click();
         WaitUtils.waitFor(1);
     }
 
     @Given("user clicks on the menu button")
-    public void user_clicks_on_the_menu_button() {
+    public void user_clicks_on_the_menu_button() throws IOException {
         viceDeanMngPage.menuButton.click();
         WaitUtils.waitFor(1);
+        MediaUtils.takeScreenshotOfTheEntirePage();
     }
 
     @And("user  selects Lesson Management on the main page")
@@ -61,18 +66,18 @@ public class ViceDeanMngCreateLesson {
 
 
     @And("user enters the lessonName and creditScore")
-    public void userEntersTheLessonNameAndCreditScore(){
+    public void userEntersTheLessonNameAndCreditScore() throws IOException {
 
         int fakeLessonName1 =new Faker().number().numberBetween(0,10);
         int fakeCreditScore1 =new Faker().number().numberBetween(0,500);
-        viceDeanMngPage.lessonName.sendKeys("abc"+fakeLessonName1);
+        viceDeanMngPage.lessonName.sendKeys("son"+fakeLessonName1);
         WaitUtils.waitFor(2);
-viceDeanMngPage.creditScore.sendKeys(fakeCreditScore1+"");
+        viceDeanMngPage.creditScore.sendKeys(fakeCreditScore1+"");
         WaitUtils.waitFor(2);
     }
 
     @And("user marks whether the course is compulsory or not")
-    public void userMarksWhetherTheCourseIsCompulsoryOrNot() {
+    public void userMarksWhetherTheCourseIsCompulsoryOrNot() throws IOException {
         viceDeanMngPage.compulsoryCheckBox.click();
         WaitUtils.waitFor(2);
     }
@@ -90,17 +95,17 @@ viceDeanMngPage.creditScore.sendKeys(fakeCreditScore1+"");
     }
 
     @And("user enters the lessonName as number and special character and creditScore")
-    public void userEntersTheLessonNameAsNumberAndSpecialCharacterAndCreditScore() {
+    public void userEntersTheLessonNameAsNumberAndSpecialCharacterAndCreditScore() throws IOException {
         int fakeLessonName =new Faker().number().numberBetween(0,14);
         int fakeCreditScore =new Faker().number().numberBetween(0,500);
-        viceDeanMngPage.lessonName.sendKeys("?%"+fakeLessonName);
+        viceDeanMngPage.lessonName.sendKeys("?!"+fakeLessonName);
         WaitUtils.waitFor(2);
         viceDeanMngPage.creditScore.sendKeys(fakeCreditScore+"");
         WaitUtils.waitFor(2);
     }
 
     @Then("assert the success alert is not displayed")
-    public void assertTheSuccessAlertIsNotDisplayed() {
+    public void assertTheSuccessAlertIsNotDisplayed() throws IOException {
         String alertText = viceDeanMngPage.alert.getText();
         assertFalse(alertText.contains("Lesson Created"));
         WaitUtils.waitFor(3);
@@ -108,19 +113,15 @@ viceDeanMngPage.creditScore.sendKeys(fakeCreditScore1+"");
 
 
     @And("user enters the lessonName and creditScore as other than numbers between zero and five hundred")
-    public void userEntersTheLessonNameAndCreditScoreAsOtherThanNumbersBetweenZeroAndFiveHundred() {
+    public void userEntersTheLessonNameAndCreditScoreAsOtherThanNumbersBetweenZeroAndFiveHundred() throws IOException {
         int fakeLessonName =new Faker().number().numberBetween(0,14);
         int fakeCreditScore =new Faker().number().numberBetween(-500,0);
-        viceDeanMngPage.lessonName.sendKeys("math"+fakeLessonName);
+        viceDeanMngPage.lessonName.sendKeys("mat"+fakeLessonName);
         WaitUtils.waitFor(2);
         viceDeanMngPage.creditScore.sendKeys(fakeCreditScore+"");
         WaitUtils.waitFor(2);
     }
 
-    @Then("close the application")
-    public void closeTheApplication() {
-        Driver.closeDriver();
-    }
 
 
 }
